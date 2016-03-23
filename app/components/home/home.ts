43af5e54f4a0ea658f1fc4aac39b1f4bb61afd27
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2016-03-23 19:24:35 +0530

    getting error:Cannot resolve all parameters for PackageList(?). Make sure they all have valid type or annotations.

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -1,11 +1,18 @@
-import {Component, View} from 'angular2/angular2';
+/// <reference path="../../../tsd_typings/angular2/angular2.d.ts"/>
+import {Component, View, NgFor, NgIf} from 'angular2/angular2';
 import {RouterLink} from 'angular2/router';
+import {PackageList} from '../../services/packages/PackageList';
 
 @Component({
-    selector: 'home-component'
+    selector: 'home-component',
+    bindings: [PackageList]
 })
 @View({
-    templateUrl: './components/home/home.html?v=<%= VERSION %>',
+    templateUrl: './components/home/home.html',
     directives: [RouterLink]
 })
-export class Home {}
+export class Home {
+  constructor(public packagelist: PackageList) {
+    console.log( packagelist.getPackageContent() );
+  }
+}
--- app/services/packages/PackageList.ts
+++ app/services/packages/PackageList.ts
@@ -1,17 +1,28 @@
 import {Injectable} from 'angular2/angular2';
 import {Http, Headers} from 'angular2/http';
 
-
 @Injectable()
 export class PackageList{
   http: Http;
+  contentData: Array<Object>;
   constructor(http: Http) {
-    console.log('Package list constructor');
+    console.warn('Package list constructor');
     this.http = http;
   }
   getContent() {
     return this.http.get('http://dynamopackages.com/packages/').then(function(response) {
       return response.json();
     });
+  }
+  getPackageContent(){
+    return this.http.get('http://dynamopackages.com/packages/')
+      .map((data: any) => data.json())
+      .subscribe(
+              (data: any) => {
+                  this.contentData = data.content;
+              },
+              err => console.log(err), // error
+              () => console.log('getPackageContent Complete') // complete
+          );
   }
 }
